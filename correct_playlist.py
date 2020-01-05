bcffd1252d1aa256ada7e777f4f049228f21bfd8
Konstas Marmatakis <marmako@gmail.com>
2020-01-06 00:21:35 +0200

    My first commit.

---
+++ correct_playlist.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import urllib.parse
+
+
+def correct_content(old_playlist='/home/km/Μουσική/xristougenniatika_new.m3u', pat='Christmas_.+'):
+
+    # with open(old_playlist, "r", encoding="UTF-8") as fh:
+    #     text = fh.read()
+    fh = open(old_playlist, "r")
+    text = fh.read()
+    fh.close()
+
+    results = re.findall(pat, text)
+    newtext = text
+
+    for res in results:
+        newtext = newtext.replace(res, urllib.parse.unquote(res))
+
+    answer = input("Θέλετε να σώσετε την νέα Λίστα με το ίδιο όνομα (yes/no); [no] ")
+    if answer.strip() == 'yes':
+        print("Γράφω το αρχείο {}".format(old_playlist))
+        fh = open(old_playlist, 'w')
+        fh.write(newtext)
+        fh.close()
+    else:
+        answer = ''
+        d, f = os.path.split(old_playlist)
+        answer = str(input("Δώστε όνομα αρχείου [προκαθορισμένο: {}]: ".format('new_{}'.format(f))))
+        if answer:
+            new_playlist = os.path.join(d, '{}'.format(answer.strip()))
+        else:
+            new_playlist = os.path.join(d, 'new_{}'.format(f))
+        print("Γράφω το αρχείο {}.".format(new_playlist))
+        fh = open(new_playlist, "w")
+        fh.write(newtext)
+        fh.close()
+    # else:
+    #     d, f = os.path.split(old_playlist)
+    #     print(d, f)
+    #     print("Γράφω το αρχείο {}".format(os.path.join(d, 'new_'+f))
+    #     # fh1 = open(os.path.join(d, 'new_'+f), "w")
+    #     # fh1.write(newtext)
+    #     # fh1.close()
+
+
+if __name__ == "__main__":
+    correct_content()
---
+++ transfer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import shutil
+import urllib.parse
+
+
+def move_files():
+    fh = open('/home/km/Μουσική/xristougenniatika_new.m3u', 'r')
+    text = fh.read()
+    fh.close()
+
+    pat = '/media/.+'
+    results = re.findall(pat, text, re.MULTILINE)
+    if not os.path.exists('/home/km/Μουσική/Christmas_Songs'):
+        os.makedirs('/home/km/Μουσική/Christmas_Songs')
+    for i in results:
+        # str_test = str_test.replace(i, urllib.parse.unquote(i))
+        newf = urllib.parse.unquote(i)
+        d, f = os.path.split(newf)
+        shutil.copy2(newf, os.path.join('/home/km/Μουσική/Christmas_Songs', f))
+
+
+if __name__ == '__main__':
+    move_files()
