9eb48022bcd791cac4fed769f512419c4a996667
Konstas Marmatakis <marmako@gmail.com>
2020-01-09 21:22:33 +0200

    Added some modifications.

--- transfer.py
+++ transfer.py
@@ -6,25 +6,32 @@ import re
 import shutil
 import urllib.parse
 
-# TODO: Add parameters.
-def move_files():
+def move_files(path_to_playlist='/home/km/Μουσική/xristougenniatika_new.m3u', pat = '/media/.+'):
 
     """
     Transfers music files from path of playlist to specific folder.
+    @path_to_playlist:  The path of the playlist.
+    @pat:               The pattern (regex) to search.
     """
-# TODO: Rewrite with 'with' form.
-    fh = open('/home/km/Μουσική/xristougenniatika_new.m3u', 'r')
-    text = fh.read()
-    fh.close()
 
-    pat = '/media/.+'
+    with open(path_to_playlist) as fh:
+        text = fh.read()
+
+    # fh = open(path_to_playlist, 'r')
+    # fh.read()
+    # fh.close()
+
+    pat = pat
     results = re.findall(pat, text, re.MULTILINE)
     if not os.path.exists('/home/km/Μουσική/Christmas_Songs'):
         os.makedirs('/home/km/Μουσική/Christmas_Songs')
+
     for i in results:
         # str_test = str_test.replace(i, urllib.parse.unquote(i))
         newf = urllib.parse.unquote(i)
         d, f = os.path.split(newf)
+        print("Copy {0} to {1}".format(os.path.join(d, f), \
+            os.path.join('/home/km/Μουσική/Christmas_Songs', f)))
         shutil.copy2(newf, os.path.join('/home/km/Μουσική/Christmas_Songs', f))
 
 
